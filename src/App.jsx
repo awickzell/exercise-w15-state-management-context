0f9b90f4355d164aeb4fc15957e6a740879eeb49
Arvid Wickzell <arvidwickzell345@hotmail.com>
2025-01-28 15:30:13 +0100

    add more code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
-import { ThemeProvider } from "./context/ThemeContext"; // Importera ThemeProvider
+import { ThemeProvider } from "./context/ThemeContext";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
